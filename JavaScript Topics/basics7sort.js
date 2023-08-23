041259edd34e81de0faf1a4d5fb5f069b59df15d
Tilak <tilakakumarp@gmail.com>
2023-08-23 14:33:05 +0530

    sorting method

---
+++ JavaScript Topics/basics7sort.js
@@ -0,0 +1,9 @@
+var names=["Amar","Akbar","Ram","Raj","Dom"]
+names.sort()
+console.log(names)
+
+var num=[12,25,45,65,32,11,21,77]
+console.log(num.sort())
+
+var num1=[012,25,45,65,32,9,02,77]
+console.log(num1.sort())
