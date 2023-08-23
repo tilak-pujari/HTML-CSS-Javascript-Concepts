7aaccca100daa6f5975ed4397aca5626609249ed
Tilak <tilakakumarp@gmail.com>
2023-08-23 15:34:30 +0530

    functionst

---
+++ JavaScript Topics/basics8func.js
@@ -0,0 +1,21 @@
+var num=[10,21,152,35,45,13]
+console.log(num)
+
+//block of code
+function add(a,b){
+    return a+b
+}
+let sum=add(2,3)
+console.log(sum)
+
+//Anonymous function --> function with no names and with expression
+
+let sumOfTwo=function(a,b){
+    return a+b
+}
+//short form
+let sumOfNums=(a,b)=>a+b
+//wrong way to use
+//console.log(sumOfNums)
+console.log(sumOfNums(5,8))
+console.log(sumOfTwo(5,8))
--- JavaScript Topics/basics8function.js
+++
@@ -1,3 +0,0 @@
-
-var num=[10,21,152,35,45,13]
-console.log(num)
