5a04d65daa6de9a747e9083d61606646ea4fac48
Tilak <tilakakumarp@gmail.com>
2023-08-23 19:19:11 +0530

    scope of variable in javascript

--- JavaScript Topics/basics9scope.js
+++ JavaScript Topics/basics9scope.js
@@ -3,32 +3,36 @@
 //var is global level/functional level
 //let is global level/block level
 
-var greet="Morning"
-if(true){
-    greet="Noon"
+var greet="Morning var"
+if(1==1){
+   var  greet="Noon var"
 }
-
 console.log(greet)
 
+//here greet is declared but not used outside function
+function hello(a,b){
+   greet="in function var"
+    return a+b
+}
+hello(2,3)
+console.log(greet)
+
+
+let greeting="Morning let"
+greeting="Evening let"
+if(1==1){
+   let greeting="Noon let"
+}
 function hello(){
-    greet="in function"
-}
-console.log(greet)
-
-
-let greeting="Afternoon"
-greeting="Evening"
-if(true){
-    greeting="Noon"
+  let  greeting="in function let"
 }
 console.log(greeting)
 
 const greets="Morning Const"
-greets="Evening const"
-if(true){
-    greets="Noon Const"
-}
+//we cant reinitialize const values
+// greets="Evening const"
+// if(true){
+//   const  greets="Noon Const"
+// }
 
-function hello(){
-    greet="in function"
-}
+console.log(greets)
