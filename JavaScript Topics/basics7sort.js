834e858fc6c13570d599e18cd41c10274d422f0a
Tilak <tilakakumarp@gmail.com>
2023-08-23 15:06:35 +0530

    sort ascending descending order

--- JavaScript Topics/basics7sort.js
+++ JavaScript Topics/basics7sort.js
@@ -4,17 +4,21 @@ var names=["Amar","Akbar","Ram","Raj","Dom"]
 names.sort()
 console.log(names)
 
-var num=[12,25,45,65,32,11,21,77]
-console.log(num.sort())
-
+// var num=[12,25,45,65,32,11,21,77]
+// console.log(num.sort())
 //we cant sort numbers directly
 var num1=[012,25,45,65,32,9,02,77]
 console.log(num1.sort())
 
-//Buble sort in recursive manner
-
+//Buble sort in recursive manner ascending order
 // num1.sort(function(a,b){
 //     return a-b
 // })
-
+//Single line code
 console.log(num1.sort((a,b)=>a-b))
+
+//Buble sort in recursive manner ascending order
+console.log(num1.sort((a,b)=>b-a))
+
+//print names in reverse descending order
+console.log(names.reverse())
