3fc365e5263ddd0c1972a84c68fdf891713000ae
Tilak <tilakakumarp@gmail.com>
2023-08-24 18:03:19 +0530

    added string manipulation methods

--- JavaScript Topics/basics10string.js
+++ JavaScript Topics/basics10string.js
@@ -20,3 +20,37 @@ console.log(splitDay[1])
 //remove leading and trailing whitespaces
 let trimmedDay=day.trim()
 console.log(trimmedDay)
+
+let number1="55"
+let number2='25'
+
+//use parseInt to convert String into integers
+let add=parseInt(number1)+parseInt(number2)
+console.log(add)
+//it works now but ideally use parseInt
+let sub=number1-number2
+console.log(sub)
+
+//convert object to string
+console.log(sub.toString())
+
+//cancatenate two strings
+let newString=day+"is first day"
+console.log(newString)
+
+//to search substring present in string
+//it will give index of first matching element
+console.log(newString.indexOf('day'))
+//to start at any point we can give position at indexOf method
+console.log(newString.indexOf('day',6))
+
+//write a program to check in 'sunday is first day' how many times 'day' occured
+
+let value=newString.indexOf('day')
+count=0
+while(value!=-1)
+{
+    count++
+    value=newString.indexOf('day',value+1)
+}
+console.log('day occured '+count)
