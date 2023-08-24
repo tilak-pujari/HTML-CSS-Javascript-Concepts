39bf4a00bca74dd329b474aee04a429c1a554397
Tilak <tilakakumarp@gmail.com>
2023-08-24 17:38:27 +0530

    string manipulation methods commit 2

--- JavaScript Topics/basics10string.js
+++ JavaScript Topics/basics10string.js
@@ -1,1 +1,22 @@
 //String manipulation methods
+
+let day="sunday "
+
+//to get length of string
+console.log(day.length)
+
+//to get character from string
+console.log(day[2])
+
+//to get substring 
+let sliceDay=day.slice(0,3)
+console.log(sliceDay)
+
+//splits string at given character
+let splitDay=day.split("n")
+console.log(splitDay[0])
+console.log(splitDay[1])
+
+//remove leading and trailing whitespaces
+let trimmedDay=day.trim()
+console.log(trimmedDay)
